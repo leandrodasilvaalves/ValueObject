6bc49aa021fb6a310e9d5ac198a01f67a39cc451
@leandro.alves <leandro.alves@s2it.com.br>
2018-06-20 12:37:04 -0300

    Add Site Implementation and Tests

---
+++ ValueObjects.PT-br.Testes/Web/SiteTestes.cs
@@ -0,0 +1,27 @@
+﻿using ValueObjects.PT_br.Web;
+using Xunit;
+
+namespace ValueObjects.PT_br.Testes.Web
+{
+    public class SiteTestes : TesteBase<Site>
+    {
+        [Theory]   
+        [InlineData("https://www.google.com.br","")]
+        [InlineData("http://www.google.com.br", "")]
+        [InlineData("https://www.google.com", "")]
+        [InlineData("wwwgooglecombr", "O site informado é inválido")]
+        [InlineData("https://.com.br", "O site informado é inválido")]
+        [InlineData(".com.br", "O site informado é inválido")]
+        public void EnderecoSite(string enderecoSite, string mensagemExperada)
+        {
+            // Arrage
+            var site = new Site(enderecoSite);
+
+            // Act
+            site.EstaValido();
+
+            // Assert
+            AssertMensagemExperada(mensagemExperada, site);
+        }
+    }
+}
---
+++ src/ValueObjects.PT-br/Web/Site.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using FluentValidation;
+
+namespace ValueObjects.PT_br.Web
+{
+    public class Site : ValueObjectBase<Site>
+    {
+        public string EnderecoSite { get; private set; }
+
+        public Site(string enderecoSite) => this.EnderecoSite = enderecoSite;
+
+        protected override void Validar()
+        {
+            ValidarEnderecoSite();
+            ResultadoValidacao = Validate(this);
+        }
+
+        private void ValidarEnderecoSite()
+        {
+            RuleFor(e => e.EnderecoSite)
+                .Must(ValidarSite).WithMessage("O site informado é inválido");
+        }
+
+        private bool ValidarSite(string enderecoSite) =>
+            Uri.IsWellFormedUriString(enderecoSite, UriKind.Absolute);
+        
+    }
+}
